50feecccbef42b7468dbceb9daa181b8a91ca21d
Devran <devranmalik82@gmail.com>
2024-11-21 09:54:28 +0700

    feat: creat encryption

--- Day-32/fe-react-redux-practice-todolist-main/src/redux/store.js
+++ Day-32/fe-react-redux-practice-todolist-main/src/redux/store.js
@@ -3,15 +3,24 @@ import { composeWithDevTools } from "@redux-devtools/extension";
 import {thunk} from "redux-thunk";
 import {persistReducer, persistStore} from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import {encryptTransform} from "redux-persist-transform-encrypt";
 import todosDataReducer from "./async/todo/reducer";
 import langReducer from "./lang/reducer";
 import themeReducer from "./theme/reducer";
+
+const encryptor = encryptTransform({
+  secretKey: import.meta.env.VITE_ENCRYPT_KEY,
+  onError: (error) => {
+    console.error("Encryption error:", error);
+  },
+});
 
 // config redux persist
 const persistConfig = {
   key: "root",
   storage,
   whitelist: ["todos"],
+  transforms: [encryptor],
 }
 
 const rootReducer = combineReducers({
