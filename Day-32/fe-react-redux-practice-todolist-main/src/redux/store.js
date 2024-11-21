21a53a6cd68ba59993809b0c2c2ad82cbef85f8d
Devran <devranmalik82@gmail.com>
2024-11-21 09:31:51 +0700

    refactor: delete unnecessary files

--- Day-32/fe-react-redux-practice-todolist-main/src/redux/store.js
+++ Day-32/fe-react-redux-practice-todolist-main/src/redux/store.js
@@ -1,15 +1,26 @@
 import { legacy_createStore as createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "@redux-devtools/extension";
 import {thunk} from "redux-thunk";
+import {persistReducer, persistStore} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import todosDataReducer from "./async/todo/reducer";
 import langReducer from "./lang/reducer";
 import themeReducer from "./theme/reducer";
+
+// config redux persist
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["todos"],
+}
 
 const rootReducer = combineReducers({
   todos: todosDataReducer,
   lang: langReducer,
   theme: themeReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // eslint-disable-next-line no-unused-vars
 const logMiddleware = store => next => action => {
@@ -18,8 +29,10 @@ const logMiddleware = store => next => action => {
 };
 
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(thunk, logMiddleware))
 );
 
-export default store;
+const persistor = persistStore(store);
+
+export {store, persistor};
